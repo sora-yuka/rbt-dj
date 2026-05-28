fe4102108df6d28047f71b52eef5ff5f3436be6a
sora-yuka <feelingjeez@gmail.com>
2026-05-28 18:37:48 +0600

    ops: Added entrypoint dependency for dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+echo "Waiting for database..."
+while ! nc -z db 5432; do
+    sleep 0.1
+done
+echo "Database started"
+
+echo "Applying database migrations..."
+python manage.py makemigrations
+python manage.py migrate
+
+echo "Collecting fixed data and static files..."
+python manage.py loaddata fixtures/users.json
+python manage.py loaddata fixtures/categories.json
+python manage.py loaddata fixtures/offers.json
+python manage.py loaddata fixtures/deals.json
+python manage.py collectstatic --noinput
+
+exec "$@"
