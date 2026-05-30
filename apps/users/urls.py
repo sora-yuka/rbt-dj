5ab3b690d61c15f3a71864f472e24256034736bc
sora-yuka <feelingjeez@gmail.com>
2026-05-30 21:34:09 +0600

    chore(users/urls): Change endpoint name

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterAPIView
 
 urlpatterns = [
-    path("enroll/", RegisterAPIView.as_view(), name="user_register"),
-    path("authenticate/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("register/", RegisterAPIView.as_view(), name="user_register"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh_pair")
 ]
