cddbc9fee4595f3d8f885ffe8172433034a76170
sora-yuka <feelingjeez@gmail.com>
2026-06-01 11:33:31 +0600

    chore: Extended access token life time

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
